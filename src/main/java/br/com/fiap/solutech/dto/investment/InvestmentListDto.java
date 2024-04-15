db7328bce61d4e75436a241cc4a252eb2a368387
LucasSpinelli003 <lucaszk003@gmail.com>
2024-04-14 23:39:15 -0300

    [Feature] - investment service - implement with relationship

--- src/main/java/br/com/fiap/solutech/dto/investment/InvestmentListDto.java
+++ src/main/java/br/com/fiap/solutech/dto/investment/InvestmentListDto.java
@@ -1,12 +1,24 @@
 package br.com.fiap.solutech.dto.investment;
 
+import br.com.fiap.solutech.dto.agency.AgencyDetailDto;
+import br.com.fiap.solutech.dto.user.UserDetailDto;
+import br.com.fiap.solutech.model.Agency;
 import br.com.fiap.solutech.model.Investment;
 import br.com.fiap.solutech.model.User;
 
-public record InvestmentListDto(Long id, String name, String type, Double value, Long idAgency, Long idUser) {
+import java.util.List;
+
+public record InvestmentListDto (Long id, String name, String type, Double value, AgencyDetailDto agency, List<UserDetailDto> user) {
 
     public InvestmentListDto(Investment investment){
-        this(investment.getId(), investment.getName(), investment.getType(), investment.getValue(), investment.getIdAgency(), investment.getIdUser());
+        this(
+                investment.getId(),
+                investment.getName(),
+                investment.getType(),
+                investment.getValue(),
+                new AgencyDetailDto(investment.getAgency()),
+                investment.getUsers().stream().map(UserDetailDto::new).toList()
+        );
     }
 
 }
--- src/main/java/br/com/fiap/solutech/dto/investment/InvestmentRegisterDto.java
+++ src/main/java/br/com/fiap/solutech/dto/investment/InvestmentRegisterDto.java
@@ -1,26 +1,24 @@
 package br.com.fiap.solutech.dto.investment;
 
+import br.com.fiap.solutech.model.Agency;
+import br.com.fiap.solutech.model.User;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+
+import java.util.List;
 
 public record InvestmentRegisterDto(
 
    @NotBlank(message = "Name field cannot be empty!")
    @Size(max = 100, min = 3, message = "The minimum name size is 3 and the maximum is 100!")
    String name,
-
    @NotBlank(message = "Mail field cannot be empty!")
    @Size(max = 150, min = 6, message = "The minimum type size is 15 and the maximum is 150!")
    String type,
-
    @NotNull(message = "Value field cannot be empty!")
    Double value,
-
-   @NotNull(message = "Agency id cannot be empty!")
-   Long idAgency,
-
    @NotNull(message = "User id cannot be empty!")
-   Long idUser
+   List<Long> usersId
 
 ) {}
